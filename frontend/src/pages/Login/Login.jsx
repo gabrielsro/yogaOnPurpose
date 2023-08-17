15fc3bd0a79f6c21f99a34bab944f06014715c7a
Gabriel Rodriguez <gabrielsro182@gmail.com>
2023-08-17 19:23:50 -0400

    Added frontend Login page

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,6 +1,18 @@
 import "./Login.css";
 
 const Login = () => {
-  return <div className="page">Login</div>;
+  return (
+    <div className="page" id="loginPage">
+      <form action="" id="loginForm">
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" />
+        <label htmlFor="username">Password:</label>
+        <input type="password" id="password" name="password" />
+        <div className="buttonContainer">
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 export default Login;
