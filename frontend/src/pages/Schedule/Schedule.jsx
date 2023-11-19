b9e3a54414a577686d192efa0c167ff05ba7b0f4
Gabriel Rodriguez <gabrielsro182@gmail.com>
2023-11-18 19:22:35 -0500

    Changed schedule title

--- frontend/src/pages/Schedule/Schedule.jsx
+++ frontend/src/pages/Schedule/Schedule.jsx
@@ -1,6 +1,13 @@
 import "./Schedule.css";
 
 const Schedule = () => {
-  return <div className="page">Schedule</div>;
+  return (
+    <div className="page">
+      <div className="schedulePageTitle">
+        <h1>Under Construction</h1>
+      </div>
+      <div className="schedulePageImg"></div>
+    </div>
+  );
 };
 export default Schedule;
