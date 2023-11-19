8cd681ee1e532f1dfe9d32aa66e7e0d599af77dc
Gabriel Rodriguez <gabrielsro182@gmail.com>
2023-11-18 19:33:50 -0500

    Schedule page aesthetics

--- frontend/src/pages/Schedule/Schedule.jsx
+++ frontend/src/pages/Schedule/Schedule.jsx
@@ -1,12 +1,17 @@
 import "./Schedule.css";
+import constructionIcon from "./icons/construction.svg";
 
 const Schedule = () => {
   return (
     <div className="page">
-      <div className="schedulePageTitle">
-        <h1>Under Construction</h1>
+      <div className="schedulePageContainer">
+        <div className="schedulePageTitle">
+          <h1>Under Construction</h1>
+        </div>
+        <div className="schedulePageImg">
+          <img src={constructionIcon} alt="Construction Icon" />
+        </div>
       </div>
-      <div className="schedulePageImg"></div>
     </div>
   );
 };
